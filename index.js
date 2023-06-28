ed6bf65bed2cac28c141110a58a84a82314c7cd4
Erix Gutierrez <erix.gutierrez@gmail.com>
2023-06-28 16:43:55 -0400

    cors fix

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ connectDB();
 const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(credentials);
-app.get("/", (req, res) => {
+app.get("/login", (req, res) => {
   console.log(req);
 });
 app.use(cors(corsOptions));

feea6212f41b089986ce49c51ae345835f9fb31a
Erix Gutierrez <erix.gutierrez@gmail.com>
2023-06-29 11:28:56 -0400

    cors fix

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -60,7 +60,6 @@ const handleLogin = async (req, res) => {
 };
 
 const handleNativeAppLogin = async (req, res) => {
-  console.log(req.body);
   const { email, password } = req.body;
   if (!email || !password)
     return res
--- middleware/verifyJWT.js
+++ middleware/verifyJWT.js
@@ -1,21 +1,21 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 const verifyJWT = (req, res, next) => {
-    console.log(req.headers)
-    const authHeader = req.headers.Authorization || req.headers.authorization;
-    if (!authHeader?.startsWith('Bearer ')) return res.status(401).json({ message: "You are unauthorized" });
-    const token = authHeader.split(' ')[1];
-    
-    jwt.verify(
-        token,
-        process.env.ACCESS_TOKEN,
-        (err, decoded) => {
-            if (err) return res.sendStatus(403); 
-            console.log(decoded.UserInfo)
-            req.user = decoded.UserInfo.username;
-            next();
-        }
-    );
-}
+  console.log(req.headers);
+  const authHeader = req.headers.Authorization || req.headers.authorization;
+  if (!authHeader?.startsWith("Bearer "))
+    return res.status(401).json({ message: "You are unauthorized" });
+  const token = authHeader.split(" ")[1];
 
-module.exports = verifyJWT
+  jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+    if (err) return res.sendStatus(403);
+    console.log(
+      decoded.UserInfo,
+      "decodeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeed"
+    );
+    req.user = decoded.UserInfo.email;
+    next();
+  });
+};
+
+module.exports = verifyJWT;
